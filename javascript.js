d08167adc4afcc1342b2147ed109bdf0393e24f1
Errina <schaertlcl@gmail.com>
2021-03-13 14:23:46 -0800

    Add a few sample questions

--- javascript.js
+++ javascript.js
@@ -1,3 +1,24 @@
 window.onload = function () {
-	document.getElementById('testing').innerHTML = "Hello world";
+	new bootstrap.Collapse(document.getElementById('question1'));
+}
+
+function dataChosen(questionNumber) {
+	document.querySelector("#question" + questionNumber + " select").disabled = true;
+	new bootstrap.Collapse(document.getElementById("question" + questionNumber + "data"));
+
+	var selectedIndex = document.querySelector("#question" + questionNumber + " select").selectedIndex;
+	new bootstrap.Collapse(document.getElementById("question" + questionNumber + "option" + selectedIndex));
+}
+
+function optionChosen(questionNumber, answerIndex) {
+	let nextDiv = "final";
+	if (questionNumber <= 5) {
+		nextDiv = "question" + (questionNumber + 1);
+	}
+	new bootstrap.Collapse(document.getElementById(nextDiv));
+
+	setTimeout(function () {
+		document.getElementById(nextDiv).scrollIntoView();
+	}, 400);
+
 }
