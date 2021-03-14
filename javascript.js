a5af89ee503a91fcfd5facf3d43fb74e54c99b36
Errina <schaertlcl@gmail.com>
2021-03-13 16:31:51 -0800

    Add the rest of the questions

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+var score = 0;
+
 window.onload = function () {
 	new bootstrap.Collapse(document.getElementById('question1'));
 }
@@ -16,16 +18,40 @@ function optionChosen(questionNumber, answerIndex) {
 	new bootstrap.Collapse(document.getElementById("question" + questionNumber + "choice" + answerIndex));
 	new bootstrap.Collapse(document.getElementById("question" + questionNumber + "results"));
 
-	let nextDiv = "final";
-	if (questionNumber <= 5) {
-		nextDiv = "question" + (questionNumber + 1);
+	if ((questionNumber === 1 && answerIndex === 0) ||
+		(questionNumber === 2 && answerIndex === 1) ||
+		(questionNumber === 3 && answerIndex === 1) ||
+		(questionNumber === 4 && answerIndex === 1) ||
+		(questionNumber === 5 && answerIndex === 0) ||
+		(questionNumber === 6 && answerIndex === 1)) {
+		score++;
 	}
 
-	new bootstrap.Collapse(document.getElementById(nextDiv));
-
+	if (questionNumber <= 5) {
+		new bootstrap.Collapse(document.getElementById("question" + (questionNumber + 1)));
+	} else {
+		var resultText = "";
+		switch (score) {
+			case 0:
+				resultText = "You didn't use the data well to make great decisions.";
+				break;
+			case 1:
+			case 2:
+			case 3:
+				resultText = "You made some of the right decisions, but there's room for improvement.";
+				break;
+			case 4:
+			case 5:
+				resultText = "You made a lot of the right decisions, though some could have been better.";
+				break;
+			case 6:
+				resultText = "You made all the right decisions, thanks to data!";
+				break;
+		}
+		document.getElementById("results").innerHTML = resultText;
+	}
 
 	setTimeout(function () {
 		document.getElementById("question" + questionNumber + "results").scrollIntoView();
 	}, 400);
-
 }
