c6b12ff32db3bd07df501ded74571fbd553a3eb4
Errina <schaertlcl@gmail.com>
2021-03-13 19:46:43 -0800

    Add charts for question 1

--- javascript.js
+++ javascript.js
@@ -1,4 +1,13 @@
 var score = 0;
+const backgroundColors = [
+	'rgba(255, 99, 132, 1)',
+	'rgba(54, 162, 235, 1)',
+	'rgba(255, 206, 86, 1)',
+	'rgba(75, 192, 192, 1)',
+	'rgba(153, 102, 255, 1)',
+	'rgba(255, 159, 64, 1)',
+	'rgba(75, 159, 64, 1)'
+];
 
 window.onload = function () {
 	new bootstrap.Collapse(document.getElementById('question1'));
@@ -9,7 +18,7 @@ function dataChosen(questionNumber) {
 	new bootstrap.Collapse(document.getElementById("question" + questionNumber + "data"));
 
 	var selectedIndex = document.querySelector("#question" + questionNumber + " select").selectedIndex;
-	new bootstrap.Collapse(document.getElementById("question" + questionNumber + "option" + selectedIndex));
+	drawChart(questionNumber, selectedIndex);
 }
 
 function optionChosen(questionNumber, answerIndex) {
@@ -54,4 +63,93 @@ function optionChosen(questionNumber, answerIndex) {
 	setTimeout(function () {
 		document.getElementById("question" + questionNumber + "results").scrollIntoView();
 	}, 400);
+}
+
+function drawChart(questionNumber, answerIndex) {
+	var ctx = document.getElementById("question" + questionNumber + "chart").getContext('2d');
+	var chartCode = questionNumber + "-" + answerIndex;
+
+	switch (chartCode) {
+		case "1-1":
+			new Chart(ctx, {
+				type: 'pie',
+				data: {
+					labels: ['Sally the Siamese', 'Toby the Terrier', 'Barry the Bulldog', 'Fred the Frog', 'Sabrina the Teenage Kit', 'Hugh the Husky', 'Other Pets'],
+					datasets: [{
+						data: [12486, 19486, 3678, 5146, 44658, 2513, 3788],
+						backgroundColor: backgroundColors,
+						borderColor: "white",
+						borderWidth: 1
+					}]
+				},
+				options: {
+					legend: {
+						position: 'bottom',
+					},
+					title: {
+						display: true,
+						text: "How Many Times Each Pet Has Been Tapped",
+					}
+				}
+			});
+			break;
+		case "1-2":
+			new Chart(ctx, {
+				type: 'pie',
+				data: {
+					labels: ['Sally the Siamese', 'Toby the Terrier', 'Barry the Bulldog', 'Fred the Frog', 'Sabrina the Teenage Kit', 'Hugh the Husky', 'Other Pets'],
+					datasets: [{
+						data: [1246, 1946, 367, 516, 44, 253, 378],
+						backgroundColor: backgroundColors,
+						borderColor: "white",
+						borderWidth: 1
+					}]
+				},
+				options: {
+					legend: {
+						position: 'bottom',
+					},
+					title: {
+						display: true,
+						text: "Number of Users That Have Tapped On Each Pet",
+					}
+				}
+			});
+			break;
+		case "1-3":
+			// Some options create images, not charts.
+			document.getElementById("question" + 1 + "chart").style.display = 'none'
+			new bootstrap.Collapse(document.getElementById("question" + questionNumber + "option" + answerIndex));
+			break;
+		case "2-1":
+			break;
+		case "2-2":
+			break;
+		case "2-3":
+			break;
+		case "3-1":
+			break;
+		case "3-2":
+			break;
+		case "3-3":
+			break;
+		case "4-1":
+			break;
+		case "4-2":
+			break;
+		case "4-3":
+			break;
+		case "5-1":
+			break;
+		case "5-2":
+			break;
+		case "5-3":
+			break;
+		case "6-1":
+			break;
+		case "6-2":
+			break;
+		case "6-3":
+			break;
+	}
 }
